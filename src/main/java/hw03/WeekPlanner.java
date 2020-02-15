daecc2d5cfb80f69199ab8cdd98023a3f03017dd
Celilkhan <celil.ahmad@gmail.com>
2020-02-15 12:52:58 +0400

    WeekPlanner

--- src/main/java/hw03/WeekPlanner.java
+++ src/main/java/hw03/WeekPlanner.java
@@ -1,5 +1,6 @@
 package hw03;
 
+
 import java.util.Scanner;
 
 public class WeekPlanner {
@@ -29,17 +30,17 @@ public class WeekPlanner {
         schedule[6][1] = "Relax whole day";
 
         Scanner sc = new Scanner(System.in);
-        //System.out.println("Please enter day of the week");
-        //String task = sc.nextLine();
+
         String beginning = "Your task for ";
         boolean test = false;
-        //String task1="";
+        String exit = "Exit";
 
         while (test == false) {
             System.out.println("Please enter day of the week");
-            String task = sc.nextLine();
-            //task.toLowerCase();
-            //task1.equalsIgnoreCase(task);
+            String task1 = sc.nextLine().trim();
+            String task = task1.substring(0, 1).toUpperCase() + task1.substring(1).toLowerCase();
+
+
             if (task.equals(schedule[0][0]) || task.equals(schedule[1][0]) || task.equals(schedule[2][0]) || task.equals(schedule[3][0])
                     || task.equals(schedule[4][0]) || task.equals(schedule[5][0]) || task.equals(schedule[6][0])) {
 
@@ -66,17 +67,13 @@ public class WeekPlanner {
                         System.out.println(beginning + task + " " + schedule[0][1]);
                         break;
                 }
-            } else if (task.equals("exit")) {
+            } else if (task.equals(exit)) {
                 test = true;
             } else {
                 System.out.println("Sorry, I don't understand you, please try again.");
             }
         }
-        /*WILL
-        BE
-        UPDATED
-        TO CASE SENSITIVE
-        SOON!     */
+
     }
 
 
