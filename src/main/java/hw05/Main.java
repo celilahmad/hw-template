6f7a9d49a613855a1fed2914f9943a414bee65b4
Celilkhan <celil.ahmad@gmail.com>
2020-02-19 12:35:07 +0400

    HappyFamily

---
+++ src/main/java/hw05/Main.java
@@ -0,0 +1,38 @@
+package hw05;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        Human mother = new Human();
+        mother.setName("Jane");
+        mother.setSurname("Karleone");
+
+        Human father = new Human();
+        father.setName("Vito");
+        father.setSurname("Karleone");
+
+
+        Pet dog = new Pet("dog","Rock",5,new String[]{"eat","drink","sleep"});
+
+        Human child = new Human("Michael","Karleone",1990,90);
+
+
+
+        System.out.println(dog);
+        System.out.println(child);
+
+        System.out.println();
+
+        //String feed= child.isFeed(false);
+        //System.out.println(feed);
+
+        child.describePet(dog);
+
+
+
+
+    }
+}
---
+++ src/main/java/hw05/Pet.java
@@ -0,0 +1,89 @@
+package hw05;
+
+import java.util.Arrays;
+
+public class Pet {
+
+    private String species;
+    private String nickName;
+    private int age;
+    private int trickLevel=(int)(Math.random()*100+1);
+    private String[] habbits;
+
+    public Pet(){}
+
+    public Pet(String species, String nickName) {
+        this.species = species;
+        this.nickName = nickName;
+    }
+
+    public Pet(String species, String nickName, int age, String[] habbits) {
+        this.species = species;
+        this.nickName = nickName;
+        this.age = age;
+
+        this.habbits = habbits;
+    }
+
+    public void eat(){
+        System.out.println("I'm eating");
+    }
+
+    public void respond(Pet pet){
+        System.out.println("Hello, owner. I am - " + pet + ". I miss you!");
+    }
+
+    public void foul(){
+        System.out.println("I need to cover up");
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setage(int age) {
+        this.age = age;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String[] getHabbits() {
+        return habbits;
+    }
+
+    public void setHabbits(String[] habbits) {
+        this.habbits = habbits;
+    }
+
+    @Override
+    public String toString() {
+        return species +"{" +
+                "nickName='" + nickName + '\'' +
+                ", age=" + age +
+                ", trickLevel=" + trickLevel +
+                ", habbits=" + Arrays.toString(habbits) +
+                '}';
+    }
+}
